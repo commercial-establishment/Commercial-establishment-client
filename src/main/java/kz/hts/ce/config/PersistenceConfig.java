bfd9c927b11ad346d2c5929ffbe83d921b13e48b
commercial-establishment <commercial.establishment@gmail.com>
2015-11-05 09:11:51 +0600

    Commit

--- src/main/java/kz/hts/ce/config/PersistenceConfig.java
+++ src/main/java/kz/hts/ce/config/PersistenceConfig.java
@@ -17,9 +17,9 @@ public class PersistenceConfig {
     public DriverManagerDataSource dataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
         driverManagerDataSource.setDriverClassName("org.h2.Driver");
-        driverManagerDataSource.setUrl("jdbc:postgresql://localhost:5432/test");
-        driverManagerDataSource.setUsername("postgres");
-        driverManagerDataSource.setPassword("root");
+        driverManagerDataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+        driverManagerDataSource.setUsername("test");
+        driverManagerDataSource.setPassword("test");
         return driverManagerDataSource;
     }
 }
