922999625a5723ddff556d108258e19dbcd2dce7
alikhann <alikhannugmanov@gmail.com>
2015-11-05 11:59:59 +0600

    Message

---
+++ src/main/java/kz/hts/ce/entity/ProductProvider.java
@@ -0,0 +1,68 @@
+package kz.hts.ce.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product_provider")
+public class ProductProvider extends BaseEntity {
+
+    @NotNull
+    private long amount;
+
+    @NotNull
+    private BigDecimal price;
+
+    @ManyToOne
+    @PrimaryKeyJoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
+    @ManyToOne
+    @PrimaryKeyJoinColumn(name = "provider_id", referencedColumnName = "id")
+    private Provider provider;
+
+    @Column(name = "is_blocked")
+    private boolean blocked;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Provider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(Provider provider) {
+        this.provider = provider;
+    }
+
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+    }
+}
+
