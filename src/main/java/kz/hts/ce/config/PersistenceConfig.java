814edfdfabada05ae37623c6c2f2bff95aa42e46
commercial-establishment <commercial.establishment@gmail.com>
2015-11-05 10:34:37 +0600

    JavaFX HELLO WORLD!

--- src/main/java/kz/hts/ce/config/PersistenceConfig.java
+++ src/main/java/kz/hts/ce/config/PersistenceConfig.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableTransactionManagement(proxyTargetClass = true)
@@ -21,5 +26,21 @@ public class PersistenceConfig {
         driverManagerDataSource.setUsername("test");
         driverManagerDataSource.setPassword("test");
         return driverManagerDataSource;
+    }
+
+
+    @Bean(name = "entityManagerFactory")
+    public EntityManagerFactory entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setPackagesToScan("kz.hts.ce");
+        emf.setPersistenceUnitName("primary");
+        emf.setDataSource(dataSource());
+        emf.afterPropertiesSet();
+        return emf.getObject();
+    }
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager(entityManagerFactory());
     }
 }
---
+++ src/main/java/kz/hts/ce/controller/MainController.java
@@ -0,0 +1,23 @@
+//package kz.hts.ce.controller;
+//
+//import javafx.event.EventHandler;
+//import javafx.fxml.FXML;
+//import javafx.scene.control.Button;
+//import javafx.scene.input.MouseEvent;
+//
+//public class MainController {
+//
+//    @FXML
+//    private Button button;
+//
+//    @FXML
+//    public void initialize(){
+//        button.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent mouseEvent) {
+//                button.setText("Thanks!");
+//            }
+//        });
+//    }
+//
+//}
