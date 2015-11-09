c162adbc6edb523a6be57515a60858620b86e433
commercial-establishment <commercial.establishment@gmail.com>
2015-11-09 09:18:11 +0600

    Login is working

--- src/main/resources/META-INF/db/script.sql
+++ src/main/resources/META-INF/db/script.sql
@@ -38,5 +38,5 @@ VALUES (1, 'ул. Ташенова 5/2', FALSE, 'COMPANY NAME', 'PERSON NAME', '
 INSERT INTO product_provider (id, is_blocked, product_id, provider_id, amount, price) VALUES (1, FALSE, 1, 1, 10, 100);
 INSERT INTO product_provider (id, is_blocked, product_id, provider_id, amount, price) VALUES (2, FALSE, 2, 1, 20, 200);
 
-INSERT INTO EMPLOYEE (ID, IS_BLOCKED, FIRST_NAME, PASSWORD, SURNAME, USERNAME, ROLE_ID, SHOP_ID) VALUES (1, FALSE, 'FIRSTNAME','employee', 'surname', 'employee', 3, 1);
+INSERT INTO EMPLOYEE (ID, IS_BLOCKED, FIRST_NAME, PASSWORD, SURNAME, USERNAME, ROLE_ID, SHOP_ID) VALUES (1, FALSE, 'FIRSTNAME','$2a$10$5/0IiLwNrICc3Dmq/7AWKO08qK13AKH2tWIytGe9a2.WZHIj2WwPa', 'surname', 'employee', 3, 1);
 -- INSERT INTO shop (id, is_blocked, full_name, password, username, type_id, warehouse_id) VALUES (1, FALSE, 'Super Магаз', 'super', 'super', 1, 1);
