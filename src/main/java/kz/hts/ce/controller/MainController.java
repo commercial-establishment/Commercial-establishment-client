7af763268d1429132927a47ace8efa9f8c0e27ea
commercial-establishment <commercial.establishment@gmail.com>
2015-11-09 09:31:47 +0600

    Merge branch 'alikhan'
    # Conflicts:
    #	src/main/java/kz/hts/ce/Main.java

--- src/main/java/kz/hts/ce/controller/MainController.java
+++ src/main/java/kz/hts/ce/controller/MainController.java
@@ -5,6 +5,7 @@ import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.util.Duration;
 import kz.hts.ce.config.FXMLDialog;
@@ -18,13 +19,16 @@ import java.util.Calendar;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-import static kz.hts.ce.util.SpringUtils.getPrincipal;
-
 @Component
 public class MainController implements DialogController, Initializable{
 
-    public Label lblUsername, dateLabel;
+    //    public Label lblUsername;
+    public Label dateLabel;
+    public Button button;
     private FXMLDialog dialog;
+
+    @Autowired
+    private ScreensConfiguration screens;
 
     //region TODO Digital watch realization
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
@@ -33,13 +37,9 @@ public class MainController implements DialogController, Initializable{
     Timeline watch = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
         @Override
         public void handle(ActionEvent event) {
-//            System.out.println("this is called every 5 seconds on UI thread");
             dateLabel.setText(simpleDateFormat.format(Calendar.getInstance().getTime()));
         }
     }));
-
-    @Autowired
-    private ScreensConfiguration screens;
 
     public MainController(ScreensConfiguration screens) {
         this.screens = screens;
@@ -51,7 +51,6 @@ public class MainController implements DialogController, Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        lblUsername.setText(getPrincipal());
         watch.setCycleCount(Timeline.INDEFINITE);
         watch.play();
     }
