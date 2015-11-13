e0ecb2c1710fd86011ef5a4afd3f9378c7a6a137
yakovshmidt <yakovshmidt93@gmail.com>
2015-11-13 17:37:37 +0600

    Added deleteProduct from table

--- src/main/java/kz/hts/ce/controller/PaymentController.java
+++ src/main/java/kz/hts/ce/controller/PaymentController.java
@@ -3,19 +3,26 @@ package kz.hts.ce.controller;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import kz.hts.ce.config.PagesConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import java.math.BigDecimal;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import static kz.hts.ce.util.JavaUtil.stringToBigDecimal;
 import static kz.hts.ce.util.SpringFxmlLoader.getPagesConfiguration;
 
 @Controller
-public class PaymentController {
+public class PaymentController implements Initializable {
 
+    public static final String ZERO = "0.00";
+    @FXML
+    private TextField shortage;
     @FXML
     private TextField given;
     @FXML
@@ -28,38 +35,43 @@ public class PaymentController {
     private Button cancel;
     @FXML
     private Button print;
+    @Autowired
+    private ProductsController productsController;
 
-    @FXML
-    private void initialize(){
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        total.setText(productsController.getPriceResult().getText());
+        shortage.setText("-" + total.getText());
+        given.setText(ZERO);
+        change.setText(ZERO);
         given.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                BigDecimal totalBD = stringToBigDecimal(getTotal().getText());
-
-                if(!newValue.equals("")) {
+                BigDecimal totalBD = stringToBigDecimal(total.getText());
+                if (!newValue.equals("")) {
                     BigDecimal newVal = stringToBigDecimal(newValue);
-                    if(newVal.compareTo(totalBD)==1) {
+                    if (newVal.compareTo(totalBD) == 1) {
                         BigDecimal changeBD = newVal.subtract(totalBD);
                         change.setText(String.valueOf(changeBD));
+                        shortage.setText(ZERO);
+                    } else {
+                        BigDecimal shortageBD = newVal.subtract(totalBD);
+                        shortage.setText(String.valueOf(shortageBD));
+                        change.setText(ZERO);
                     }
-                    else
-                        change.setText("");
                 }
             }
         });
     }
 
     @FXML
-    private void cancel(){
+    private void cancel() {
         PagesConfiguration screens = getPagesConfiguration();
         screens.payment().close();
-    }
-
-    public TextField getTotal() {
-        return total;
     }
 
     public TextField getGiven() {
         return given;
     }
+
 }
