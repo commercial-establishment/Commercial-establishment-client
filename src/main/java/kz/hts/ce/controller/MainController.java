05b2d98302a79ec72db538b013a098e14430c718
yakovshmidt <yakovshmidt93@gmail.com>
2015-11-16 14:45:02 +0600

    Added logout

--- src/main/java/kz/hts/ce/controller/MainController.java
+++ src/main/java/kz/hts/ce/controller/MainController.java
@@ -1,23 +1,43 @@
 package kz.hts.ce.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import kz.hts.ce.config.PagesConfiguration;
+import kz.hts.ce.model.entity.Employee;
+import kz.hts.ce.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import static kz.hts.ce.util.JavaFxUtil.getWatch;
+import static kz.hts.ce.util.SpringFxmlLoader.getPagesConfiguration;
+import static kz.hts.ce.util.SpringUtil.getPrincipal;
 
 @Controller
 public class MainController implements Initializable {
 
-    public Label dateLabel;
-    public Button button;
+    @FXML
+    private Label dateLabel;
+    @FXML
+    private Label role;
+
+    @Autowired
+    private EmployeeService employeeService;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         getWatch(dateLabel);
+        Employee employee = employeeService.findByUsername(getPrincipal());
+        role.setText(employee.getRole().getName());
+    }
+
+    public void logout(ActionEvent event) {
+        PagesConfiguration screens = getPagesConfiguration();
+        screens.main().close();
+        screens.login().show();
     }
 }
