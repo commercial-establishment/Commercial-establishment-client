8a30f85d88daf42f70d2f4d61d9fcfaa8b5bbb75
alikhann <alikhannugmanov@gmail.com>
2015-11-26 11:47:03 +0600

    created AddReceiptController

---
+++ src/main/java/kz/hts/ce/controller/AddReceiptController.java
@@ -0,0 +1,21 @@
+package kz.hts.ce.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import kz.hts.ce.config.PagesConfiguration;
+import org.springframework.stereotype.Controller;
+
+import static kz.hts.ce.util.SpringFxmlLoader.getPagesConfiguration;
+
+@Controller
+public class AddReceiptController {
+
+    @FXML
+    public void handle(){
+        PagesConfiguration screens = getPagesConfiguration();
+        Stage stage = new Stage();
+        screens.setPrimaryStage(stage);
+        screens.addReceipt();
+    }
+}
