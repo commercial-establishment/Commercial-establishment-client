6eccd08adf984e4dbd72c72d33331f884196b26d
alikhann <alikhannugmanov@gmail.com>
2015-12-03 08:51:27 +0600

    fixed painting row by residue

--- src/main/java/kz/hts/ce/controller/SettingsController.java
+++ src/main/java/kz/hts/ce/controller/SettingsController.java
@@ -25,28 +25,26 @@ public class SettingsController implements Initializable{
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         minValue.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10000, 0));
-        maxValue.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10000, 70));
-//        min = minValue.getValue();
-//        max = maxValue.getValue();
+        maxValue.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10000, 0));
     }
     @FXML
     public void update(){
         String textMin = minValue.getEditor().getText();
         SpinnerValueFactory<Integer> valueFactoryMin = minValue.getValueFactory();
         if (valueFactoryMin != null) {
-            StringConverter<Integer> converter = valueFactoryMin.getConverter();
-            if (converter != null) {
-                Integer valueMin = converter.fromString(textMin);
+            StringConverter<Integer> converterMin = valueFactoryMin.getConverter();
+            if (converterMin != null) {
+                Integer valueMin = converterMin.fromString(textMin);
                 valueFactoryMin.setValue(valueMin);
                 min = valueFactoryMin.getValue();
             }
         }
         String textMax = maxValue.getEditor().getText();
-        SpinnerValueFactory<Integer> valueFactoryMax = minValue.getValueFactory();
+        SpinnerValueFactory<Integer> valueFactoryMax = maxValue.getValueFactory();
         if (valueFactoryMax != null) {
-            StringConverter<Integer> converter = valueFactoryMax.getConverter();
-            if (converter != null) {
-                Integer valueMax = converter.fromString(textMax);
+            StringConverter<Integer> converterMax = valueFactoryMax.getConverter();
+            if (converterMax != null) {
+                Integer valueMax = converterMax.fromString(textMax);
                 valueFactoryMax.setValue(valueMax);
                 max = valueFactoryMax.getValue();
             }
