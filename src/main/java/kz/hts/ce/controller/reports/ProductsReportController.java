a6b9b6053dac46d64a331d89f4351f0119a7fba0
alikhann <alikhannugmanov@gmail.com>
2015-12-09 16:40:08 +0600

    .

--- src/main/java/kz/hts/ce/controller/reports/ProductsReportController.java
+++ src/main/java/kz/hts/ce/controller/reports/ProductsReportController.java
@@ -2,10 +2,15 @@ package kz.hts.ce.controller.reports;
 
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
+import javafx.util.Callback;
 import kz.hts.ce.model.dto.ProductDto;
 import kz.hts.ce.model.entity.Category;
 import kz.hts.ce.model.entity.Product;
@@ -14,10 +19,9 @@ import kz.hts.ce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-
 import java.math.BigDecimal;
-import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ProductsReportController {
@@ -26,9 +30,9 @@ public class ProductsReportController {
     @FXML
     private DatePicker endDate;
     @FXML
-    private TreeTableView<String> productsReport;
+    private TreeTableView productsReport;
     @FXML
-    private TreeTableColumn<Category, ProductDto> categoryProduct;
+    private TreeTableColumn<ProductDto, String> categoryProduct;
     @FXML
     private TreeTableColumn<ProductDto, Number> amount;
     @FXML
@@ -42,9 +46,9 @@ public class ProductsReportController {
     @FXML
     private TreeTableColumn<ProductDto, BigDecimal> shopPrice;
 
-
-    private TreeItem<String> categoryTreeItem = new TreeItem<>();
-    private TreeItem<String> productTreeItem = new TreeItem<>();
+    private TreeItem<ProductDto> parentRoot = new TreeItem<>();
+    private TreeItem<Map<Category, List<ProductDto>>> categoryTreeItem = new TreeItem<>();
+    private TreeItem<ProductDto> productTreeItem = new TreeItem<>();
 
 
     private ObservableList<ProductDto> productsData = FXCollections.observableArrayList();
@@ -58,22 +62,25 @@ public class ProductsReportController {
     public void showReport() {
 //        if (!startDate.getEditor().getText().equals("") && !endDate.getEditor().getText().equals("")){
 //        }
-        List<Category> categories = categoryService.findAll();
-        categoryTreeItem.setExpanded(true);
-        TreeItem<String> name = new TreeItem<>();
-        for (Category category : categories) {
-            name.setValue(category.getName());
-            categoryTreeItem.setValue(category.getName());
-            categoryTreeItem.getChildren().addAll(name);
-        }
-//        TreeItem<String> ch1 = new TreeItem<>();
-//        TreeItem<String> ch2 = new TreeItem<>();
-//        ch1.setValue("1");
-//        ch2.setValue("2");
-//        categoryTreeItem.setExpanded(true);
-//        categoryTreeItem.getChildren().setAll(ch1, ch2);
-//        categoryProduct.setCellValueFactory((TreeTableColumn.CellDataFeatures<String, String> p) -> new ReadOnlyStringWrapper(
-//                p.getValue().getValue()));
-//        productsReport.setRoot(categoryTreeItem);
+        List<Category> all = categoryService.findAll();
+        List<Product> productList = productService.findAll();
+        parentRoot.setExpanded(true);
+
+        ProductDto productDto = new ProductDto();
+        productDto.setName("asdasdasd");
+        productDto.setAmount(111);
+
+
+
+        categoryTreeItem.getChildren().add(new TreeItem<>());
+//        categoryTreeItem.setValue("");
+//        parentRoot.getChildren().add(categoryTreeItem);
+//        parentRoot.getChildren().add(new TreeItem<>(all.get(1).getName()));
+
+        categoryProduct.setCellValueFactory((TreeTableColumn.CellDataFeatures<ProductDto, String> p) ->
+                new ReadOnlyStringWrapper(p.getValue().getValue().getName()));
+        amount.setCellValueFactory(p -> new ReadOnlyObjectWrapper<>(p.getValue().getValue().getAmount()));
+        productsReport.setRoot(parentRoot);
+        productsReport.setShowRoot(false);
     }
 }
