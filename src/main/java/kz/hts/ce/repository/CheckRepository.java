81bdea41da23702b2ca4f0e929303beab8e46b40
alikhann <alikhannugmanov@gmail.com>
2016-01-25 16:35:29 +0600

    added findByDates and last date

--- src/main/java/kz/hts/ce/repository/CheckRepository.java
+++ src/main/java/kz/hts/ce/repository/CheckRepository.java
@@ -1,10 +1,12 @@
 package kz.hts.ce.repository;
 
 import kz.hts.ce.model.entity.Check;
+import kz.hts.ce.model.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,6 +14,10 @@ import java.util.UUID;
 public interface CheckRepository extends JpaRepository<Check, UUID> {
 
     List<Check> findByCheckNumber(String checkNumber);
+
+    List<Check> findByDateBetween(Date start, Date end);
+
+    List<Check> findByEmployee_Username(String username);
 
     @Query("SELECT c FROM Check c WHERE c.date IN (SELECT max(date) FROM Check)")
     Check findByLastDate();
--- src/main/java/kz/hts/ce/service/CheckService.java
+++ src/main/java/kz/hts/ce/service/CheckService.java
@@ -1,10 +1,12 @@
 package kz.hts.ce.service;
 
 import kz.hts.ce.model.entity.Check;
+import kz.hts.ce.model.entity.Employee;
 import kz.hts.ce.repository.CheckRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -18,7 +20,15 @@ public class CheckService extends BaseService<Check, CheckRepository> {
         return repository.findByCheckNumber(checkNumber);
     }
 
+    public List<Check> findByEmployeeUsername(String userName){
+        return repository.findByEmployee_Username(userName);
+    }
+
     public Check findByLastDate(){
         return repository.findByLastDate();
+    }
+
+    public List<Check> findByDateBetween(Date startDate, Date endDate){
+        return repository.findByDateBetween(startDate, endDate);
     }
 }
