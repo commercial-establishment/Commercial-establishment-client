6c742a85342649e94487a9d3a9cc1cc4ff0663dd
yakovshmidt <yakovshmidt93@gmail.com>
2016-01-25 10:28:27 +0600

    Change public const to private

--- src/main/java/kz/hts/ce/util/javafx/fields/IntegerTextField.java
+++ src/main/java/kz/hts/ce/util/javafx/fields/IntegerTextField.java
@@ -4,7 +4,7 @@ import javafx.scene.control.TextField;
 
 public class IntegerTextField extends TextField {
 
-    public static final String REGEX = "^[\\d]{0,20}$";
+    private static final String REGEX = "^[\\d]{0,20}$";
 
     @Override
     public void replaceText(int start, int end, String text) {
